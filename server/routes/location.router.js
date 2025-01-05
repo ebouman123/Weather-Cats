80ab919a59a3721242e6c03df451f37e805c3c68
Evan Bouman <evan.bouman@gmail.com>
2025-01-05 10:34:18 -0600

    Fixed location router, needed US in the url

--- server/routes/location.router.js
+++ server/routes/location.router.js
@@ -7,7 +7,7 @@ router.get("/:city/:state", async (req, res) => {
   const { city, state } = req.params;
   try {
     const response = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state}&limit=1&appid=${process.env.appid}`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=1&appid=${process.env.appid}`
     );
     res.json(response.data[0]);
   } catch (error) {
